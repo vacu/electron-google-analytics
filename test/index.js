d91c637855bff61e85f798afbea63a2b92f15b6b
Calin Stefanescu <c_stefanescu@mgcorp.co>
2017-02-07 18:45:48 +0200

    Tests

--- test/index.js
+++ test/index.js
@@ -4,15 +4,26 @@ import Analytics from '../src/index';
 const expect = chai.expect;
 
 describe('Analytics', function() {
-  it('should send a pageview request', function() {
+  it('should fail sending a pageview request', function() {
     const analytics = new Analytics('', { debug: true });
 
     return analytics.pageview('http://example.com', 'test', 'test')
     .then((response) => {
-      return expect(response).to.have.property('clientID');
+      return expect(response).to.be.empty;
     }).catch((err) => {
-      // console.log(err);
-      return expect(err).to.be.empty;
+      return expect(err).to.not.be.empty;
     });
   });
+
+  // it('should send a pageview request', function() {
+  //   const analytics = new Analytics('xxx', { debug: true });
+  //
+  //   return analytics.pageview('http://example.com', 'test', 'test')
+  //   .then((response) => {
+  //     return expect(response).to.have.property('clientID');
+  //   }).catch((err) => {
+  //     // console.log(err);
+  //     return expect(err).to.be.empty;
+  //   });
+  // });
 });
