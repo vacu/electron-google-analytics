cc4fae693378fcc278c283bcf5d48fc469893487
Calin Stefanescu <c_stefanescu@mgcorp.co>
2017-02-07 18:26:45 +0200

    Started test implementation

--- test/index.js
+++ test/index.js
@@ -1,3 +1,18 @@
 import chai from 'chai';
+import Analytics from '../src/index';
 
 const expect = chai.expect;
+
+describe('Analytics', function() {
+  it('should send a pageview request', function() {
+    const analytics = new Analytics('', { debug: true });
+
+    return analytics.pageview('http://example.com', 'test', 'test')
+    .then((response) => {
+      return expect(response).to.have.property('clientID');
+    }).catch((err) => {
+      // console.log(err);
+      return expect(err).to.be.empty;
+    });
+  });
+});
