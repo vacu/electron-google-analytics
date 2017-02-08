006db248b375f3421e374f97ed84195d2075f346
Calin Stefanescu <c_stefanescu@mgcorp.co>
2017-02-08 14:13:47 +0200

    Fixed test if tracking_id doesn't exist

--- test/index.js
+++ test/index.js
@@ -2,9 +2,56 @@ import chai from 'chai';
 import Analytics from '../src/index';
 
 const expect = chai.expect;
-const trackingID = process.env.TRACKING_ID;
+const trackingID = (process.env.TRACKING_ID) ? process.env.TRACKING_ID : '';
 
 describe('Analytics', function() {
+
+  if (trackingID) {
+    it('should send a pageview request', function() {
+      const analytics = new Analytics(trackingID, { debug: true });
+
+      return analytics.pageview('http://example.com', '/test', 'Test')
+      .then((response) => {
+        return expect(response).to.have.property('clientID');
+      }).catch((err) => {
+        return expect(err).to.be.empty;
+      });
+    });
+
+    it('should send a event request', function() {
+      const analytics = new Analytics(trackingID, { debug: true });
+
+      return analytics.event('category', 'view').then((response) => {
+        return expect(response).to.have.property('clientID');
+      }).catch((err) => {
+        return expect(err).to.be.empty;
+      });
+    });
+
+    it('should send a screenview request', function() {
+      const analytics = new Analytics(trackingID, { debug: true });
+
+      return analytics.screen('test', '1.0.0', 'com.app.test', 'com.app.installer', 'Test')
+      .then((response) => {
+        return expect(response).to.have.property('clientID');
+      }).catch((err) => {
+        return expect(err).to.be.empty;
+      });
+    });
+
+    it('should send a custom request', function() {
+      const analytics = new Analytics(trackingID, { debug: true });
+
+      return analytics.send('social', { sa: 'social', sn: 'facebook', st: 'home' })
+      .then((response) => {
+        return expect(response).to.have.property('clientID');
+      }).catch((err) => {
+        return expect(err).to.be.empty;
+      });
+    });
+  }
+
+
   it('should fail sending a pageview request', function() {
     const analytics = new Analytics('', { debug: true });
 
@@ -13,27 +60,6 @@ describe('Analytics', function() {
       return expect(response).to.be.empty;
     }).catch((err) => {
       return expect(err).to.not.be.empty;
-    });
-  });
-
-  it('should send a pageview request', function() {
-    const analytics = new Analytics(trackingID, { debug: true });
-
-    return analytics.pageview('http://example.com', '/test', 'Test')
-    .then((response) => {
-      return expect(response).to.have.property('clientID');
-    }).catch((err) => {
-      return expect(err).to.be.empty;
-    });
-  });
-
-  it('should send a event request', function() {
-    const analytics = new Analytics(trackingID, { debug: true });
-
-    return analytics.event('category', 'view').then((response) => {
-      return expect(response).to.have.property('clientID');
-    }).catch((err) => {
-      return expect(err).to.be.empty;
     });
   });
 
@@ -47,17 +73,6 @@ describe('Analytics', function() {
     });
   });
 
-  it('should send a screenview request', function() {
-    const analytics = new Analytics(trackingID, { debug: true });
-
-    return analytics.screen('test', '1.0.0', 'com.app.test', 'com.app.installer', 'Test')
-    .then((response) => {
-      return expect(response).to.have.property('clientID');
-    }).catch((err) => {
-      return expect(err).to.be.empty;
-    });
-  });
-
   it('should fail sending a screenview request', function() {
     const analytics = new Analytics('', { debug: true });
 
@@ -66,17 +81,6 @@ describe('Analytics', function() {
       return expect(response).to.be.empty;
     }).catch((err) => {
       return expect(err).to.not.be.empty;
-    });
-  });
-
-  it('should send a custom request', function() {
-    const analytics = new Analytics(trackingID, { debug: true });
-
-    return analytics.send('social', { sa: 'social', sn: 'facebook', st: 'home' })
-    .then((response) => {
-      return expect(response).to.have.property('clientID');
-    }).catch((err) => {
-      return expect(err).to.be.empty;
     });
   });
 
