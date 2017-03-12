c43b7a7292fd983fef7f3261255f1364e79c448b
mehdi <mehdi@stash.pm>
2017-03-12 02:10:35 +0100

    Standardized the code + removed lib from repo

--- src/index.js
+++ src/index.js
@@ -1,292 +1,292 @@
-import request from 'request';
-import uuidV4 from 'uuid/v4';
+import request from 'request'
+import uuidV4 from 'uuid/v4'
 
 class Analytics {
   /**
    * Class constructor
    */
-  constructor(trackingID, { userAgent = '', debug = false, version = 1 } = {}) {
+  constructor (trackingID, { userAgent = '', debug = false, version = 1 } = {}) {
     // Debug
-    this._debug = debug;
+    this._debug = debug
 
     // User-agent
-    this._userAgent = userAgent;
+    this._userAgent = userAgent
 
     // Links
-    this._baseURL = 'https://www.google-analytics.com';
-    this._debugURL = '/debug';
-    this._collectURL = '/collect';
-    this._batchURL = '/batch';
+    this._baseURL = 'https://www.google-analytics.com'
+    this._debugURL = '/debug'
+    this._collectURL = '/collect'
+    this._batchURL = '/batch'
 
     // Google generated ID
-    this._trackingID = trackingID;
+    this._trackingID = trackingID
     // Google API version
-    this._version = version;
+    this._version = version
   }
 
-  get debug() {
-    return this._debug;
+  get debug () {
+    return this._debug
   }
 
-  set debug(value) {
-    this._debug = value;
+  set debug (value) {
+    this._debug = value
   }
 
-  get userAgent() {
-    return this._userAgent;
+  get userAgent () {
+    return this._userAgent
   }
 
-  set userAgent(value) {
-    this._userAgent = value;
+  set userAgent (value) {
+    this._userAgent = value
   }
 
-  get baseURL() {
-    return this._baseURL;
+  get baseURL () {
+    return this._baseURL
   }
 
-  set baseURL(value) {
-    this._baseURL = value;
+  set baseURL (value) {
+    this._baseURL = value
   }
 
-  get debugURL() {
-    return this._debugURL;
+  get debugURL () {
+    return this._debugURL
   }
 
-  set debugURL(value) {
-    this._debugURL = value;
+  set debugURL (value) {
+    this._debugURL = value
   }
 
-  get collectURL() {
-    return this._collectURL;
+  get collectURL () {
+    return this._collectURL
   }
 
-  set collectURL(value) {
-    this._collectURL = value;
+  set collectURL (value) {
+    this._collectURL = value
   }
 
-  get batchURL() {
-    return this._batchURL;
+  get batchURL () {
+    return this._batchURL
   }
 
-  set batchURL(value) {
-    this._batchURL = value;
+  set batchURL (value) {
+    this._batchURL = value
   }
 
-  get trackingID() {
-    return this._trackingID;
+  get trackingID () {
+    return this._trackingID
   }
 
-  set trackingID(value) {
-    this._trackingID = value;
+  set trackingID (value) {
+    this._trackingID = value
   }
 
-  get version() {
-    return this._version;
+  get version () {
+    return this._version
   }
 
-  set version(value) {
-    this._version = value;
+  set version (value) {
+    this._version = value
   }
 
   /**
    * Send a "pageview" request
    *
-   * @param  {string} url      Url of the page
-   * @param  {string} title    Title of the page
-   * @param  {string} hostname Document hostname
-   * @param  {string} clientID uuidV4
+   * @param {string} url Url of the page
+   * @param {string} title Title of the page
+   * @param {string} hostname Document hostname
+   * @param {string} [clientID] uuidV4
    *
    * @return {Promise}
    */
-  pageview(hostname, url, title, clientID) {
-    const params = { dh: hostname, dp: url, dt: title };
-    return this.send('pageview', params, clientID);
+  pageview (hostname, url, title, clientID) {
+    const params = { dh: hostname, dp: url, dt: title }
+    return this.send('pageview', params, clientID)
   }
 
   /**
    * Send a "event" request
    *
-   * @param  {string} evCategory Event category
-   * @param  {string} evAction   Event action
-   * @param  {string} clientID   uuidV4
-   * @param  {string} evLabel    Event label
-   * @param  {string} evValue    Event description
+   * @param {string} evCategory Event category
+   * @param {string} evAction Event action
+   * @param {string} [clientID]   uuidV4
+   * @param {string} evLabel Event label
+   * @param {string} evValue Event description
    *
    * @return {Promise}
    */
-  event(evCategory, evAction, { evLabel, evValue, clientID } = {}) {
-    let params = { ec: evCategory, ea: evAction };
+  event (evCategory, evAction, { evLabel, evValue, clientID } = {}) {
+    let params = { ec: evCategory, ea: evAction }
 
-    if (evLabel) params['el'] = evLabel;
-    if (evValue) params['ev'] = evValue;
+    if (evLabel) params['el'] = evLabel
+    if (evValue) params['ev'] = evValue
 
-    return this.send('event', params, clientID);
+    return this.send('event', params, clientID)
   }
 
   /**
    * Send a "screenview" request
    *
-   * @param  {string} appName        App name
-   * @param  {string} appVer         App version
-   * @param  {string} appID          App Id
-   * @param  {string} appInstallerID App Installer Id
-   * @param  {string} screenName     Screen name / Content description
-   * @param  {string} clientID       uuidV4
+   * @param {string} appName App name
+   * @param {string} appVer App version
+   * @param {string} appID App Id
+   * @param {string} appInstallerID App Installer Id
+   * @param {string} screenName Screen name / Content description
+   * @param {string} [clientID]       uuidV4
    *
    * @return {Promise}
    */
-  screen(appName, appVer, appID, appInstallerID, screenName, clientID) {
+  screen (appName, appVer, appID, appInstallerID, screenName, clientID) {
     const params = {
       an: appName,
       av: appVer,
       aid: appID,
       aiid: appInstallerID,
       cd: screenName
-    };
+    }
 
-    return this.send('screenview', params, clientID);
+    return this.send('screenview', params, clientID)
   }
 
   /**
    * Send a "transaction" request
    *
-   * @param  {string} trnID    Transaction ID
-   * @param  {string} trnAffil Transaction affiliation
-   * @param  {string} trnRev   Transaction Revenue
-   * @param  {Number} trnShip  Transaction shipping
-   * @param  {Number} trnTax   Transaction tax
-   * @param  {string} currCode Currency code
-   * @param  {string} clientID uuidV4
+   * @param {string} trnID Transaction ID
+   * @param {string} trnAffil Transaction affiliation
+   * @param {string} trnRev Transaction Revenue
+   * @param {Number} trnShip Transaction shipping
+   * @param {Number} trnTax Transaction tax
+   * @param {string} currCode Currency code
+   * @param {string} [clientID] uuidV4
    *
    * @return {Promise}
    */
-  transaction(trnID, { trnAffil, trnRev, trnShip, trnTax, currCode } = {}, clientID) {
-    let params = { ti: trnID };
+  transaction (trnID, { trnAffil, trnRev, trnShip, trnTax, currCode } = {}, clientID) {
+    let params = { ti: trnID }
 
-    if (trnAffil) params['ta'] = trnAffil;
-    if (trnRev) params['tr'] = trnRev;
-    if (trnShip) params['ts'] = trnShip;
-    if (trnTax) params['tt'] = trnTax;
-    if (currCode) params['cu'] = currCode;
+    if (trnAffil) params['ta'] = trnAffil
+    if (trnRev) params['tr'] = trnRev
+    if (trnShip) params['ts'] = trnShip
+    if (trnTax) params['tt'] = trnTax
+    if (currCode) params['cu'] = currCode
 
-    return this.send('transaction', params, clientID);
+    return this.send('transaction', params, clientID)
   }
 
   /**
    * Send a "social" request
    *
-   * @param  {string} socialAction  Social Action
-   * @param  {string} socialNetwork Social Network
-   * @param  {string} socialTarget  Social Target
-   * @param  {string} clientID      uuidV4
+   * @param {string} socialAction Social Action
+   * @param {string} socialNetwork Social Network
+   * @param {string} socialTarget Social Target
+   * @param {string} [clientID]      uuidV4
    *
    * @return {Promise}
    */
-  social(socialAction, socialNetwork, socialTarget, clientID) {
+  social (socialAction, socialNetwork, socialTarget, clientID) {
     const params = {
       sa: socialAction,
       sn: socialNetwork,
       st: socialTarget
-    };
+    }
 
-    return this.send('social', params, clientID);
+    return this.send('social', params, clientID)
   }
 
   /**
    * Send a "exception" request
    *
-   * @param  {string} exDesc   Exception description
-   * @param  {Number} exFatal  Exception is fatal?
-   * @param  {string} clientID uuidV4
+   * @param {string} exDesc Exception description
+   * @param {Number} exFatal Exception is fatal?
+   * @param {string} [clientID] uuidV4
    *
    * @return {Promise}
    */
-  exception(exDesc, exFatal, clientID) {
-    const params = { exd: exDesc, exf: exFatal };
+  exception (exDesc, exFatal, clientID) {
+    const params = { exd: exDesc, exf: exFatal }
 
-    return this.send('exception', params, clientID);
+    return this.send('exception', params, clientID)
   }
 
   /**
    * Send a "refund" request
    *
-   * @param {string} transactionID  Transaction ID
-   * @param {string} evCategory     Event category
-   * @param {string} evAction       Event action
+   * @param {string} transactionID Transaction ID
+   * @param {string} evCategory Event category
+   * @param {string} evAction Event action
    * @param {Number} nonInteraction Non-interaction parameter
-   * @param {string} clientID       uuidV4
+   * @param {string} [clientID]       uuidV4
    *
    * @returns {Promise}
    */
-  refund(transactionID, evCategory = 'Ecommerce', evAction = 'Refund', nonInteraction = 1, clientID) {
+  refund (transactionID, evCategory = 'Ecommerce', evAction = 'Refund', nonInteraction = 1, clientID) {
     const params = {
       ec: evCategory,
       ea: evAction,
       ni: nonInteraction,
       ti: transactionID,
       pa: 'refund'
-    };
+    }
 
-    return this.send('event', params, clientID);
+    return this.send('event', params, clientID)
   }
 
   /**
    * Send a request to google-analytics
    *
-   * @param  {string} hitType  Hit type
-   * @param  {string} clientID Unique identifier (uuidV4)
-   * @param  {Object} params   Options
+   * @param {string} hitType Hit type
+   * @param {Object} params Options
+   * @param {string} [clientID] Unique identifier (uuidV4)
    *
    * @return {Promise}
    */
-  send(hitType, params, clientID) {
+  send (hitType, params, clientID) {
     return new Promise((resolve, reject) => {
       let formObj = {
         v: this._version,
         tid: this._trackingID,
         cid: clientID || uuidV4(),
         t: hitType
-      };
-      if (params) Object.assign(formObj, params);
+      }
+      if (params) Object.assign(formObj, params)
 
-      let url = `${this._baseURL}${this._collectURL}`;
+      let url = `${this._baseURL}${this._collectURL}`
       if (this._debug) {
-        url = `${this._baseURL}${this._debugURL}${this._collectURL}`;
+        url = `${this._baseURL}${this._debugURL}${this._collectURL}`
       }
 
-      let reqObj = { url: url, form: formObj };
+      let reqObj = { url: url, form: formObj }
       if (this._userAgent !== '') {
-        reqObj['headers'] = { 'User-Agent': this._userAgent };
+        reqObj['headers'] = { 'User-Agent': this._userAgent }
       }
 
       return request.post(reqObj, (err, httpResponse, body) => {
-        if (err) return reject(err);
+        if (err) return reject(err)
 
-
-        let bodyJson = {};
+        let bodyJson = {}
         if (body && (httpResponse.headers['content-type'] !== 'image/gif')) {
-          bodyJson = JSON.parse(body);
+          bodyJson = JSON.parse(body)
         }
 
         if (httpResponse.statusCode === 200) {
           if (this._debug) {
             if (bodyJson.hitParsingResult[0].valid) {
-              return resolve({ clientID: formObj.cid });
+              return resolve({ clientID: formObj.cid })
             }
 
-            return reject(bodyJson);
+            return reject(bodyJson)
           }
 
-          return resolve({ clientID: formObj.cid });
+          return resolve({ clientID: formObj.cid })
         }
 
-        if (httpResponse.headers['content-type'] !== 'image/gif')
-          return reject(bodyJson);
+        if (httpResponse.headers['content-type'] !== 'image/gif') {
+          return reject(bodyJson)
+        }
 
-        return reject(body);
-      });
-    });
+        return reject(body)
+      })
+    })
   }
 }
 
-export default Analytics;
+export default Analytics
--- test/index.js
+++ test/index.js
@@ -1,177 +1,178 @@
-import chai from 'chai';
-import Analytics from '../src/index';
+'use strict'
+/* global describe, it */
 
-const expect = chai.expect;
-const trackingID = (process.env.TRACKING_ID) ? process.env.TRACKING_ID : '';
+import chai from 'chai'
+import Analytics from '../src/index'
 
-describe('Analytics', function() {
+const expect = chai.expect
+const trackingID = (process.env.TRACKING_ID) ? process.env.TRACKING_ID : ''
 
+describe('Analytics', function () {
   if (trackingID) {
-    it('should send a pageview request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a pageview request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.pageview('http://example.com', '/test', 'Test')
-      .then((response) => {
-        return expect(response).to.have.property('clientID');
-      }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        .then((response) => {
+          return expect(response).to.have.property('clientID')
+        }).catch((err) => {
+          return expect(err).to.be.empty
+        })
+    })
 
-    it('should send a event request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a event request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.event('category', 'view').then((response) => {
-        return expect(response).to.have.property('clientID');
+        return expect(response).to.have.property('clientID')
       }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        return expect(err).to.be.empty
+      })
+    })
 
-    it('should send a screenview request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a screenview request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.screen('test', '1.0.0', 'com.app.test', 'com.app.installer', 'Test')
-      .then((response) => {
-        return expect(response).to.have.property('clientID');
-      }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        .then((response) => {
+          return expect(response).to.have.property('clientID')
+        }).catch((err) => {
+          return expect(err).to.be.empty
+        })
+    })
 
-    it('should send a transaction request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a transaction request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.transaction(123).then((response) => {
-        return expect(response).to.have.property('clientID');
+        return expect(response).to.have.property('clientID')
       }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        return expect(err).to.be.empty
+      })
+    })
 
-    it('should send a social request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a social request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.social('like', 'facebook', 'home').then((response) => {
-        return expect(response).to.have.property('clientID');
+        return expect(response).to.have.property('clientID')
       }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        return expect(err).to.be.empty
+      })
+    })
 
-    it('should send a exception request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a exception request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.exception('IOException', 1).then((response) => {
-        return expect(response).to.have.property('clientID');
+        return expect(response).to.have.property('clientID')
       }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        return expect(err).to.be.empty
+      })
+    })
 
-    it('should send a refund request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a refund request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.refund('T123').then((response) => {
-        return expect(response).to.have.property('clientID');
+        return expect(response).to.have.property('clientID')
       }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        return expect(err).to.be.empty
+      })
+    })
 
-    it('should send a custom request', function() {
-      const analytics = new Analytics(trackingID, { debug: true });
+    it('should send a custom request', function () {
+      const analytics = new Analytics(trackingID, { debug: true })
 
       return analytics.send('social', { sa: 'social', sn: 'facebook', st: 'home' })
-      .then((response) => {
-        return expect(response).to.have.property('clientID');
-      }).catch((err) => {
-        return expect(err).to.be.empty;
-      });
-    });
+        .then((response) => {
+          return expect(response).to.have.property('clientID')
+        }).catch((err) => {
+          return expect(err).to.be.empty
+        })
+    })
   }
 
-
-  it('should fail sending a pageview request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a pageview request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.pageview('http://example.com', 'test', 'test')
-    .then((response) => {
-      return expect(response).to.be.empty;
-    }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      .then((response) => {
+        return expect(response).to.be.empty
+      }).catch((err) => {
+        return expect(err).to.not.be.empty
+      })
+  })
 
-  it('should fail sending a event request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a event request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.event('category', 'view').then((response) => {
-      return expect(response).to.be.empty;
+      return expect(response).to.be.empty
     }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      return expect(err).to.not.be.empty
+    })
+  })
 
-  it('should fail sending a screenview request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a screenview request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.screen('test', '1.0.0', 'com.app.test', 'com.app.installer', 'Test')
-    .then((response) => {
-      return expect(response).to.be.empty;
-    }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      .then((response) => {
+        return expect(response).to.be.empty
+      }).catch((err) => {
+        return expect(err).to.not.be.empty
+      })
+  })
 
-  it('should fail sending a transaction request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a transaction request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.transaction(123).then((response) => {
-      return expect(response).to.be.empty;
+      return expect(response).to.be.empty
     }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      return expect(err).to.not.be.empty
+    })
+  })
 
-  it('should fail sending a social request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a social request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.social('like', 'facebook', 'home').then((response) => {
-      return expect(response).to.be.empty;
+      return expect(response).to.be.empty
     }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      return expect(err).to.not.be.empty
+    })
+  })
 
-  it('should fail sending a exception request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a exception request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.exception('IOException', 1).then((response) => {
-      return expect(response).to.be.empty;
+      return expect(response).to.be.empty
     }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      return expect(err).to.not.be.empty
+    })
+  })
 
-  it('should fail sending a refund request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a refund request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.refund('T123').then((response) => {
-      return expect(response).to.be.empty;
+      return expect(response).to.be.empty
     }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
+      return expect(err).to.not.be.empty
+    })
+  })
 
-  it('should fail sending a custom request', function() {
-    const analytics = new Analytics('', { debug: true });
+  it('should fail sending a custom request', function () {
+    const analytics = new Analytics('', { debug: true })
 
     return analytics.send('social', { sa: 'social', sn: 'facebook', st: 'home' })
-    .then((response) => {
-      return expect(response).to.be.empty;
-    }).catch((err) => {
-      return expect(err).to.not.be.empty;
-    });
-  });
-});
+      .then((response) => {
+        return expect(response).to.be.empty
+      }).catch((err) => {
+        return expect(err).to.not.be.empty
+      })
+  })
+})
